752eecd7940fecbc509059c6b721d2030d4f0d01
Hayeong Kim <fibonacci2246@gmail.com>
2022-10-13 15:51:03 +0900

    [add] add todo route

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -9,6 +9,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="todo" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
